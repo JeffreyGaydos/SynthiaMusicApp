654a6d5fb3df9cf777d520703034712421d77b6a
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-04-13 00:03:45 -0400

    naiive progress bar; maybe this is the only way but who knows....

--- Synthia/Player/Player.tsx
+++ Synthia/Player/Player.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Text, View, TouchableOpacity, Image, Animated } from 'react-native';
+import { Text, View, TouchableOpacity, Image, Animated, DimensionValue } from 'react-native';
 import TrackPlayer, { AddTrack, Track } from 'react-native-track-player';
 import { s_player } from './Player'
 import Icon from 'react-native-vector-icons/Foundation';
 
+const progressRefreshRateMsec = 250;
+
 function Player({addTrack} : {addTrack: AddTrack | undefined}) {
     const [playState, setPlayState] = useState<boolean>(false);
-    const [onLoad, setOnLoad] = useState<boolean>(false);
+    const [trackProgress, setTrackProgress] = useState<number>(0);
+    const [needProgressUpdate, setNeedProgressUpdate] = useState<boolean>(true);
 
     const fadeAnim = useRef(new Animated.Value(0)).current;
 
@@ -30,6 +33,7 @@ function Player({addTrack} : {addTrack: AddTrack | undefined}) {
     }, [addTrack?.artwork]);
 
     function playpause(play: boolean | undefined = undefined) {
+        setNeedProgressUpdate(!needProgressUpdate);
         setPlayState(play === undefined ? !playState : play);
     }
 
@@ -40,12 +44,9 @@ function Player({addTrack} : {addTrack: AddTrack | undefined}) {
     useEffect(() => {
         (async () => {
             if(addTrack) {
-                console.log(onLoad);
-                if(playState && onLoad) {
-                    console.log("attempting play");
+                if(playState) {
                     await TrackPlayer.play();
                 } else {
-                    console.log("attempting pause");
                     await TrackPlayer.pause();
                 }
             } else {
@@ -62,30 +63,39 @@ function Player({addTrack} : {addTrack: AddTrack | undefined}) {
                 await TrackPlayer.reset();
                 await TrackPlayer.add(addTrack);
                 console.log(addTrack);
-                if(onLoad) {
-                    setPlayState(false);
-                } else {
-                    console.log("stopped onload play");
-                    //don't play on app load up
-                    setOnLoad(true);
-                }
+                setPlayState(false);
                 playpause(true);
             }
         })();
     }, [addTrack]);
 
-    function next() {
+    useEffect(() => {
+        (async () => {
+            if(addTrack) {
+                const getProgress = await TrackPlayer.getProgress();
+                setTrackProgress(getProgress.position / getProgress.duration);
+            }
+        })();
+    }, [needProgressUpdate]);
 
-    }
+    // this stuff is just so we can get a consistent timer that doesn't overquery if we start having trouble rendering
+    useEffect(() => {
+        setTimeout(() => {
+            setNeedProgressUpdate(!needProgressUpdate);
+        }, progressRefreshRateMsec);
+    }, [trackProgress]);
 
-    function previous() {
-
-    }
+    useEffect(() => {
+        setNeedProgressUpdate(!needProgressUpdate);
+    }, []);
 
     return (
         <>
             {addTrack && <>
-                <View style={s_player.progress}></View>
+                <View style={{
+                    ...s_player.progress,
+                    width: `${trackProgress * 100}%` as DimensionValue
+                    }}></View>
                 <View style={s_player.container}>
                     <View style={s_player.spacer}></View>
                     <View style={s_player.controls}>
@@ -93,13 +103,15 @@ function Player({addTrack} : {addTrack: AddTrack | undefined}) {
                             style={{...s_player.previous, ...s_player.controlWidget}}
                             name="previous"
                             size={30}
-                            onPress={() => console.log("TODO: PREVIOUS UNIMPLEMENTED")}
+                            onPress={() => {
+                                console.log("TODO: PREVIOUS UNIMPLEMENTED");
+                            }}
                         />
                         <Icon 
-                                style={{...s_player.playpause, ...s_player.controlWidget}}
-                                name={playState ? "pause" : "play"}
-                                size={30}
-                                onPress={PlayPausePress}
+                            style={{...s_player.playpause, ...s_player.controlWidget}}
+                            name={playState ? "pause" : "play"}
+                            size={30}
+                            onPress={PlayPausePress}
                         />
                         <Icon 
                             style={{...s_player.next, ...s_player.controlWidget}}

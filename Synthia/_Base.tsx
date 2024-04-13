46c29a8fc580039d2bbb07530779ab41617753f0
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-04-12 20:14:03 -0400

    add template files

---
+++ Synthia/_Base.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Base(/* Parameters */) {
+    function nothing() {
+        
+    }
+
+    return (
+        <View></View>
+    );
+}
+
+export default Base;
---
+++ Synthia/_BaseStyle.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const base = StyleSheet.create({
+    name: {
+        //styles here
+    }
+})
+
+export {
+    base
+}

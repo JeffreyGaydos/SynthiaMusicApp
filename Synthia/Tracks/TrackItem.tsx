8013072a93442f270b31ac25d41e4b6bac2715a0
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-07-09 22:13:32 -0400

    Update track item with name and artist plus primary color when images not found

--- Synthia/Tracks/TrackItem.tsx
+++ Synthia/Tracks/TrackItem.tsx
@@ -1,14 +1,10 @@
 import React, { useContext } from 'react';
 import {
-    Button,
     Image,
     TouchableOpacity,
   Text, View,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Foundation';
-import routes from '../routes';
 import { s_track_item } from './TrackItemStyles';
-import TrackPlayer from 'react-native-track-player';
 import { PlayerTrack } from '../MainTabs';
 import { _colors } from '../styles';
 
@@ -28,10 +24,11 @@ function TrackItem({ image, title, trackArtist, musicPath, active, setMeActive, 
     return (
         <TouchableOpacity onPress={PlayTrack} style={{...s_track_item.item, borderColor: active ? _colors._colorActive : _colors._colorAccent}}>
             <View style={{...s_track_item.icon_wrapper, borderColor: active ? _colors._colorActive : _colors._colorAccent}}>
-                <Image style={s_track_item.icon} src={"file:///" + image}></Image>
+                <Image style={{...s_track_item.icon, backgroundColor: image.startsWith("#") ? image : _colors._colorDeepAccent}} src={"file://" + image}></Image>
             </View>
             <View style={{...s_track_item.details_wrapper}}>
-                <Text numberOfLines={2}>{title}</Text>
+                <Text numberOfLines={1}>{title}</Text>
+                <Text numberOfLines={1}>{trackArtist}</Text>
             </View>
         </TouchableOpacity>
     );
